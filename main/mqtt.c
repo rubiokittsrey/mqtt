25d3ae9f0ac0f614aea6360689c464144ad5de58
Kitts Rey Rubio <kittr64@gmail.com>
2024-08-12 19:59:23 +0800

    Initial commit

---
+++ main/mqtt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#include <stdio.h> //for basic printf commands
+#include <string.h> //for handling strings
+#include "freertos/FreeRTOS.h" //for delay,mutexs,semphrs rtos operations
+#include "esp_system.h" //esp_init funtions esp_err_t 
+#include "esp_wifi.h" //esp_wifi_init functions and wifi operations
+#include "esp_log.h" //for showing logs
+#include "esp_event.h" //for wifi event
+#include "nvs_flash.h" //non volatile storage
+#include "lwip/err.h" //light weight ip packets error handling
+#include "lwip/sys.h" //system applications for light weight ip apps
+
+void app_main(void)
+{
+    
+}
